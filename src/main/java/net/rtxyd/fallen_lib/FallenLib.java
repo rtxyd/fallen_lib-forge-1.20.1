ea78a3c70b540a341e278a185c67c59b3b0ab479
rtxyd <65117312+rtxyd@users.noreply.github.com>
2025-12-20 22:01:10 +0800

    Aligned mod id and mod main class.

--- src/main/java/net/rtxyd/fallen_lib/FallenLib.java
+++ src/main/java/net/rtxyd/fallen_lib/FallenLib.java
@@ -3,9 +3,9 @@ package net.rtxyd.fallen_lib;
 import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(FallenDevHelper.MODID)
-public class FallenDevHelper
+@Mod(FallenLib.MODID)
+public class FallenLib
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "fallen_dev_helper";
+    public static final String MODID = "fallen_lib";
 }
